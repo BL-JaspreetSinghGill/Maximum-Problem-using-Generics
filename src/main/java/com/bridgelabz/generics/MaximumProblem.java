c70966487ec30da18a375183d71352fe3436f718
JaspreetSinghGill <jaspreetsg06@gmail.com>
2020-02-11 12:32:23 +0530

    REFACTORED: testmaximum method to generic method

--- src/main/java/com/bridgelabz/generics/MaximumProblem.java
+++ src/main/java/com/bridgelabz/generics/MaximumProblem.java
@@ -2,25 +2,7 @@ package com.bridgelabz.generics;
 
 public class MaximumProblem {
 
-    public static Integer testMaximum(Integer firstValue, Integer secondValue, Integer thirdValue) {
-        if (firstValue.compareTo(secondValue) > 0 && firstValue.compareTo(thirdValue) > 0) {
-            return firstValue;
-        } else if (secondValue.compareTo(thirdValue) > 0) {
-            return secondValue;
-        }
-        return thirdValue;
-    }
-
-    public static Float testMaximum(Float firstValue, Float secondValue, Float thirdValue) {
-        if (firstValue.compareTo(secondValue) > 0 && firstValue.compareTo(thirdValue) > 0) {
-            return firstValue;
-        } else if (secondValue.compareTo(thirdValue) > 0) {
-            return secondValue;
-        }
-        return thirdValue;
-    }
-
-    public static String testMaximum(String firstValue, String secondValue, String thirdValue) {
+    public static <T extends Comparable> T testMaximum(T firstValue, T secondValue, T thirdValue) {
         if (firstValue.compareTo(secondValue) > 0 && firstValue.compareTo(thirdValue) > 0) {
             return firstValue;
         } else if (secondValue.compareTo(thirdValue) > 0) {
