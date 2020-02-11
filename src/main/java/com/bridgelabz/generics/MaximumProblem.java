93735a933b5c0647363d4772c49407b4de7a8f22
JaspreetSinghGill <jaspreetsg06@gmail.com>
2020-02-11 11:59:00 +0530

    ADDED: given float number at various pos returns same max number

--- src/main/java/com/bridgelabz/generics/MaximumProblem.java
+++ src/main/java/com/bridgelabz/generics/MaximumProblem.java
@@ -10,4 +10,13 @@ public class MaximumProblem {
         }
         return thirdNumber;
     }
+
+    public static Float testMaximum(Float firstNumber, Float secondNumber, Float thirdNumber) {
+        if (firstNumber.compareTo(secondNumber) > 0 && firstNumber.compareTo(thirdNumber) > 0) {
+            return firstNumber;
+        } else if (secondNumber.compareTo(thirdNumber) > 0) {
+            return secondNumber;
+        }
+        return thirdNumber;
+    }
 }
--- src/test/java/com/bridgelabz/generics/MaximumProblemTest.java
+++ src/test/java/com/bridgelabz/generics/MaximumProblemTest.java
@@ -19,4 +19,20 @@ public class MaximumProblemTest {
     public void givenIntegerMaxNumber_WhenAtThirdPos_ShouldReturnSameNumber() {
         Assert.assertSame(50, MaximumProblem.testMaximum(30, 40, 50 ));
     }
+
+    @Test
+    public void givenFloatMaxNumber_WhenAtFirstPos_ShouldReturnSameNumber() {
+        Assert.assertEquals(3.1f, MaximumProblem.testMaximum(3.1f, 2.2f, 1.3f ), 0.0f);
+    }
+
+    @Test
+    public void givenFloatMaxNumber_WhenAtSecondPos_ShouldReturnSameNumber() {
+        Assert.assertEquals(10.5f, MaximumProblem.testMaximum(5.4f, 10.5f, 7.6f ), 0.0f);
+    }
+
+    @Test
+    public void givenFloatMaxNumber_WhenAtThirdPos_ShouldReturnSameNumber() {
+        Assert.assertEquals(50.9f, MaximumProblem.testMaximum(30.7f, 40.8f, 50.9f ), 0.0f);
+    }
 }
+
